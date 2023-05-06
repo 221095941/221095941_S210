55d1fa428ce6998ccd43f133c3e8ea8f23b0f938
221095941 <103027268+221095941@users.noreply.github.com>
2023-05-06 23:56:39 +1000

    Update task_3_2C_Code.cpp

--- Task 3.2C/task_3_2C_Code.cpp
+++ Task 3.2C/task_3_2C_Code.cpp
@@ -10,6 +10,9 @@ int light_threshold = 10000;
 // IFTTT event name
 String event_light_sensor = "Light_Sensor_Triggered";
 
+String data = "";
+
+
 void setup(){
     
     lightSensor.begin(BH1750LIB_MODE_CONTINUOUSHIGHRES);
@@ -21,17 +24,20 @@ void loop() {
     Particle.publish("Lux Measured", String(lux), PRIVATE);
     
     // IFTTT Section
-    String data = "";
     
     if (lux > light_threshold){
+        if (data = "Dark"){
         data = "Light";
-        // Trigger the integration
         Particle.publish(event_light_sensor, data, PRIVATE);
-    } else {
-        data = "Dark";
-        Particle.publish(event_light_sensor, data, PRIVATE);
+        }
+
+    } 
+    if (lux < light_threshold){
+        if (data = "Light"){
+            data = "Dark";
+            Particle.publish(event_light_sensor, data, PRIVATE);
+        }
     }
-    
     // Delay
     delay(600000);
 }
